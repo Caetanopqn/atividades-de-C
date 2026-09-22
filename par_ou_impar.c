fb74f18f0f013b7709fd6a1b188c6fde057d9086
Caetanopqn <115043671+Caetanopqn@users.noreply.github.com>
2026-09-22 14:56:02 -0300

    Adição de algoritmo "par_ou_impar.c"
    Fixed variable declaration for 'impares' and corrected syntax errors.

---
+++ par_ou_impar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+    int pares = 0;
+    impares = 0;
+
+    for (int i = 1; i <= 10; i++) {
+        printf("digite um valor: ");
+        scanf("%d", &numero);
+
+        if (numero % 2 == 0) {
+            printf("esse valor é par\n");
+            pares++;
+        } else {
+            printf("esse valor é impar\n");
+            impares++;
+        }
+
+        printf("\n//////////\n");
+    }
+
+    printf("\nquantidade de pares: %d\n", pares);
+    printf("quantidade de impares: %d\n", impares);
+
+    return 0;
+}
